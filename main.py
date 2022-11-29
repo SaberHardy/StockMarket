aa42e54ed54f7b8835f17772c26050139b271289
SaberHardy <samirsamado5@gmail.com>
2022-11-29 14:45:13 +0100

    welcome page buttons

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from tkinter import messagebox
 
 
 def Achats():
-    pass
+    root.destroy()
+    call(['python', 'Achats.py'])
 
 
 def Ventes():
-    pass
+    root.destroy()
+    call(['python', 'Ventes.py'])
 
 
 root = Tk()
@@ -20,5 +22,29 @@ root.resizable(False, False)
 root.configure(background="#808080")
 
 # Add The title
+lbl_title = Label(root, borderwidth=3,
+                  relief=SUNKEN,
+                  text="Stock Market",
+                  font=("Sans Sarif", 25),
+                  background="#483D8B",
+                  foreground="white",
+                  )
+lbl_title.place(x=0, y=0, width=600)
+
+btn_register = Button(root, text="Achats",
+                      font=("Arial", 25),
+                      bg="#483D8B",
+                      fg="red",
+                      command="achat"
+                      )
+btn_register.place(x=70, y=100, width=200)
+
+btn_register2 = Button(root, text="Ventes",
+                       font=("Arial", 25),
+                       bg="#483D8B",
+                       fg="red",
+                       command="ventes"
+                       )
+btn_register2.place(x=350, y=100, width=200)
 
 root.mainloop()
