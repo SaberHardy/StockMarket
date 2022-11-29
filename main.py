3a26ea4adb37e9af172422f69e8fcc114f325dbf
SaberHardy <samirsamado5@gmail.com>
2022-11-29 14:17:47 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from subprocess import call
+from tkinter import ttk, Tk
+from tkinter import *
+from tkinter import messagebox
+
+
+def Achats():
+    pass
+
+
+def Ventes():
+    pass
+
+
+root = Tk()
+
+root.title("Gestion Des Achats")
+root.geometry("600x200+400+200")
+root.resizable(False, False)
+root.configure(background="#808080")
+
+# Add The title
+
+root.mainloop()
