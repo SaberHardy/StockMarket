8150eb857da15c3409e9851a7cb48aefab0c903d
SaberHardy <samirsamado5@gmail.com>
2022-11-30 16:48:34 +0100

    create entries

--- SQLITE_DB_CRM/database_CRM.py
+++ SQLITE_DB_CRM/database_CRM.py
@@ -1,52 +1,54 @@
-from tkinter import messagebox
-from subprocess import call
-from tkinter import ttk, Tk
 from tkinter import *
+from tkinter import ttk
 
 root = Tk()
-root.title("CRM Actions")
-root.geometry("1000x500")
+root.title('CRM Project TreeBase')
+root.geometry("1500x500")
 
-# Add Style
+# Add Some Style
 style = ttk.Style()
-# Pick a theme
+
+# Pick A Theme
 style.theme_use('default')
-# Configure the style
+
+# Configure the Treeview Colors
 style.configure("Treeview",
                 background="#D3D3D3",
-                foreground='black',
+                foreground="black",
                 rowheight=25,
-                filedbackground="#D3D3D3")
+                fieldbackground="#D3D3D3")
 
-# Change selected color
-style.map('Treeview', background=[('selected', '#343453')])
-# Create treeview frame
+# Change Selected Color
+style.map('Treeview',
+          background=[('selected', "#347083")])
+
+# Create a Treeview Frame
 tree_frame = Frame(root)
 tree_frame.pack(pady=10)
 
-# Create a treeview Scrollbar
+# Create a Treeview Scrollbar
 tree_scroll = Scrollbar(tree_frame)
 tree_scroll.pack(side=RIGHT, fill=Y)
 
-# Create the treeview
-my_tree = ttk.Treeview(tree_scroll, yscrollcommand=tree_scroll.set, selectmode='extended')
+# Create The Treeview
+my_tree = ttk.Treeview(tree_frame, yscrollcommand=tree_scroll.set, selectmode="extended")
 my_tree.pack()
 
-# Configure the scroll
+# Configure the Scrollbar
 tree_scroll.config(command=my_tree.yview)
 
-# Define the columns
-my_tree['columns'] = ('First Name', 'Last Name', 'ID', 'City', 'Zip Code', 'State', 'Address')
+# Define Our Columns
+my_tree['columns'] = ("First Name", "Last Name", "ID", "Address", "City", "State", "Zipcode")
 
-# Format the columns
-my_tree.column('#0', width=0, stretch=NO)
+# Format Our Columns
+my_tree.column("#0", width=0, stretch=NO)
 my_tree.column("First Name", anchor=W, width=140)
 my_tree.column("Last Name", anchor=W, width=140)
-my_tree.column("ID", anchor=CENTER, width=140)
+my_tree.column("ID", anchor=CENTER, width=100)
 my_tree.column("Address", anchor=CENTER, width=140)
 my_tree.column("City", anchor=CENTER, width=140)
 my_tree.column("State", anchor=CENTER, width=140)
-my_tree.column("Zip Code", anchor=CENTER, width=140)
+my_tree.column("Zipcode", anchor=CENTER, width=140)
 
 # Create Headings
 my_tree.heading("#0", text="", anchor=W)
@@ -56,7 +58,90 @@ my_tree.heading("ID", text="ID", anchor=CENTER)
 my_tree.heading("Address", text="Address", anchor=CENTER)
 my_tree.heading("City", text="City", anchor=CENTER)
 my_tree.heading("State", text="State", anchor=CENTER)
-my_tree.heading("Zip Code", text="Zip Code", anchor=CENTER)
+my_tree.heading("Zipcode", text="Zipcode", anchor=CENTER)
 
+# Add Fake Data
+data = [
+    ["John", "Elder", 1, "123 Elder St.", "Las Vegas", "NV", "89137"],
+    ["Mary", "Smith", 2, "435 West Lookout", "Chicago", "IL", "60610"],
+    ["Tim", "Tanaka", 3, "246 Main St.", "New York", "NY", "12345"],
+    ["Erin", "Erinton", 4, "333 Top Way.", "Los Angeles", "CA", "90210"],
+    ["Bob", "Bobberly", 5, "876 Left St.", "Memphis", "TN", "34321"],
+    ["Steve", "Smith", 6, "1234 Main St.", "Miami", "FL", "12321"],
+    ["Tina", "Browne", 7, "654 Street Ave.", "Chicago", "IL", "60611"],
+    ["Mark", "Lane", 8, "12 East St.", "Nashville", "TN", "54345"],
+    ["John", "Smith", 9, "678 North Ave.", "St. Louis", "MO", "67821"],
+    ["Mary", "Todd", 10, "9 Elder Way.", "Dallas", "TX", "88948"],
+    ["John", "Lincoln", 11, "123 Elder St.", "Las Vegas", "NV", "89137"],
+    ["Mary", "Bush", 12, "435 West Lookout", "Chicago", "IL", "60610"],
+    ["Tim", "Reagan", 13, "246 Main St.", "New York", "NY", "12345"],
+    ["Erin", "Smith", 14, "333 Top Way.", "Los Angeles", "CA", "90210"],
+    ["Bob", "Field", 15, "876 Left St.", "Memphis", "TN", "34321"],
+    ["Steve", "Target", 16, "1234 Main St.", "Miami", "FL", "12321"],
+    ["Tina", "Walton", 17, "654 Street Ave.", "Chicago", "IL", "60611"],
+    ["Mark", "Erendale", 18, "12 East St.", "Nashville", "TN", "54345"],
+    ["John", "Nowerton", 19, "678 North Ave.", "St. Louis", "MO", "67821"],
+    ["Mary", "Hornblower", 20, "9 Elder Way.", "Dallas", "TX", "88948"]
+
+]
+
+# Create Striped Row Tags
+my_tree.tag_configure('oddrow', background="white")
+my_tree.tag_configure('evenrow', background="lightblue")
+
+# Add our data to the screen
+global count
+count = 0
+
+for record in data:
+    if count % 2 == 0:
+        my_tree.insert(parent='', index='end', iid=count, text='',
+                       values=(record[0], record[1], record[2], record[3], record[4], record[5], record[6]),
+                       tags=('evenrow',))
+    else:
+        my_tree.insert(parent='', index='end', iid=count, text='',
+                       values=(record[0], record[1], record[2], record[3], record[4], record[5], record[6]),
+                       tags=('oddrow',))
+    # increment counter
+    count += 1
+
+# Add Record Entry Boxes
+data_frame = LabelFrame(root, text="Record".upper())
+data_frame.pack(fill="x", expand="yes", padx=20)
+
+fn_label = Label(data_frame, text="First Name")
+fn_label.grid(row=0, column=0, padx=10, pady=10)
+fn_entry = Entry(data_frame)
+fn_entry.grid(row=0, column=1, padx=10, pady=10)
+
+ln_label = Label(data_frame, text="Last Name")
+ln_label.grid(row=0, column=2, padx=10, pady=10)
+ln_entry = Entry(data_frame)
+ln_entry.grid(row=0, column=3, padx=10, pady=10)
+
+id_label = Label(data_frame, text="ID")
+id_label.grid(row=0, column=4, padx=10, pady=10)
+id_entry = Entry(data_frame)
+id_entry.grid(row=0, column=5, padx=10, pady=10)
+
+address_label = Label(data_frame, text="Address")
+address_label.grid(row=1, column=0, padx=10, pady=10)
+address_entry = Entry(data_frame)
+address_entry.grid(row=1, column=1, padx=10, pady=10)
+
+city_label = Label(data_frame, text="City")
+city_label.grid(row=1, column=2, padx=10, pady=10)
+city_entry = Entry(data_frame)
+city_entry.grid(row=1, column=3, padx=10, pady=10)
+
+state_label = Label(data_frame, text="State")
+state_label.grid(row=1, column=4, padx=10, pady=10)
+state_entry = Entry(data_frame)
+state_entry.grid(row=1, column=5, padx=10, pady=10)
+
+zipcode_label = Label(data_frame, text="Zip code")
+zipcode_label.grid(row=1, column=6, padx=10, pady=10)
+zipcode_entry = Entry(data_frame)
+zipcode_entry.grid(row=1, column=7, padx=10, pady=10)
 
 root.mainloop()
